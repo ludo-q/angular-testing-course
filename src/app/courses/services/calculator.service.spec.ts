78b20ea2aa4ac7f08e0345fb3512dd4fb9913077
Ludvig Qerraxhija <ludvig.qerraxhija1@walgreens.com>
2024-12-06 12:38:18 +0100

    Test setup (organizations) using beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,20 +1,28 @@
 import { CalculatorService } from "./calculator.service";
-import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
-    it('should add to numbers', () => {
-        const logger = jasmine.createSpyObj('LoggerService', ['log']);
-        const calculator = new CalculatorService(logger);
+    let calculator: CalculatorService,
+        loggerSpy: any;
+
+    beforeEach(() => {
+        console.log('Calling BeforeEach');
         
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+        calculator = new CalculatorService(loggerSpy);
+    })
+    it('should add to numbers', () => {
+        console.log('add test');
         const result = calculator.add(2, 2);
         
         expect(result).toBe(4, 'unexpected add result');
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 
     it('should subtract to numbers', () => {
-        const calculator = new CalculatorService(new LoggerService);
+        console.log('subtract test');
         const result = calculator.subtract(2, 2);
+
         expect(result).toBe(0, 'unexpected subtraction result');
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 })
