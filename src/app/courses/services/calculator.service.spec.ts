1df2cf88de6f84d774d69f06cb6b76ccc2df3040
Ludvig Qerraxhija <ludvig.qerraxhija1@walgreens.com>
2024-12-06 12:25:27 +0100

    Starting to work jasmine spies

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,9 +3,13 @@ import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
     it('should add to numbers', () => {
-        const calculator = new CalculatorService(new LoggerService);
+        const logger = jasmine.createSpyObj('LoggerService', ['log']);
+        const calculator = new CalculatorService(logger);
+        
         const result = calculator.add(2, 2);
+        
         expect(result).toBe(4, 'unexpected add result');
+        expect(logger.log).toHaveBeenCalledTimes(1);
     });
 
     it('should subtract to numbers', () => {
