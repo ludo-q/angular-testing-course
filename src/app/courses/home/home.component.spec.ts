b2933c7d2c3862c9d96ed3f9a2be635869a7e61f
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2024-12-22 10:16:14 +0100

    Tested HomeComponent, display only advanced courses

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -16,6 +16,7 @@ describe('HomeComponent', () => {
   let coursesService: jasmine.SpyObj<CoursesService>;
 
   const beginnerCourses = setupCourses().filter(course => course.category == 'BEGINNER');
+  const advancedCourses = setupCourses().filter(course => course.category == 'ADVANCED');
 
   beforeEach(waitForAsync(() => {
     const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses']);
@@ -56,7 +57,11 @@ describe('HomeComponent', () => {
 
   it("should display only advanced courses", () => {
 
-      pending();
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css(".mat-mdc-tab"));
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found");
 
   });
 
