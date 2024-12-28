eb476b73c2c857e535ec9902fb5a5b0fc8c9a7c2
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2024-12-28 16:19:52 +0100

    Tested HomeComponent - E2E - on visit, currect number of mat-cart list for courses - fake apis calls/data

--- cypress/integration/0-course/home.test.js
+++ cypress/integration/0-course/home.test.js
@@ -1,6 +1,15 @@
 describe('Home Page', () => {
     it('should display a list of courses', () => {
+
+        cy.fixture('courses.json').as('coursesJSON');
+
+        cy.server();
+        cy.route('api/courses', '@coursesJSON').as('courses');
+        
         cy.visit('/');
         cy.contains('All Courses');
-    })
+
+        cy.wait('@courses');
+        cy.get('.mat-mdc-card').should('have.length', 9);
+    });
 });
